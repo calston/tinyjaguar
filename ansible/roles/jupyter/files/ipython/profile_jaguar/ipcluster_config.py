376b814d3ba3fe8fa993a15bd85f43aa86fca7a5
Colin Alston <colin.alston@gmail.com>
2017-03-12 16:20:58 +0000

    Adding jupyter configs to ansible

---
+++ ansible/roles/jupyter/files/ipython/profile_jaguar/ipcluster_config.py
@@ -0,0 +1,4 @@
+c.IPClusterEngines.engine_launcher_class = 'MPIEngineSetLauncher'
+c.IPClusterEngines.n = 4
+
+c.MPILauncher.mpi_args = ['-host','node1,node2,node3,node4']
---
+++ ansible/roles/jupyter/files/ipython/profile_jaguar/ipcontroller_config.py
@@ -0,0 +1,1 @@
+c.HubFactory.ip = '*'
